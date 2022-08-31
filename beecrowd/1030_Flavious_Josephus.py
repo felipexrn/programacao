a2d9dc671fa3c17e601ca126b9030fe2e27c40ab
felipexrn <felipe.x@escolar.ifrn.edu.br>
2022-08-31 13:05:09 +0000

    tentativa 7777

--- beecrowd/1030_Flavious_Josephus.py
+++ beecrowd/1030_Flavious_Josephus.py
@@ -1,25 +1,45 @@
 nc = int(input())
 for i in range(nc):
-  n, m = map(int, input().split())
-  roda = []
+  n, k = map(int, input().split())
+  roda=[]
   for j in range(n):
     roda.append(j+1)
-  while roda.count(0) > 1:
-    try:
-      if roda[roda.index(m+(k*m))] == m:
-        
-    except:
-      roda[roda.index(m+(k*m))] = 0
-  print(f"Case {i+1}: {max(roda)}")
+  endereco = 0
+  while len(roda) > 1:
+    if k > len(roda):
+      del roda[k+endereco-1]
+      endereco += k-1 
+    else: 
+      endereco = abs(endereco-len(roda))
+  print(f"Case {i+1}: {roda[sortudo]}")
 '''
+Casos de teste:
+
 1 2 3 4 5
+1 2 3 4
 
+1 2 3 4 5
+3 1   2 4  
 
+1 2 3 4 5
+2 4 1   3
+
+1 2 3 4 5
+  4 2 1 3
+
+1 2 3 4 5
+2   3 4 1
+
+1 2 3 4 5
+1 3 2   4
+
+Entrada:
 3
 5 2
 6 3
 1234 233
 
+Saída:
 Case 1: 3
 Case 2: 1
 Case 3: 25
