4f79477b46f57a1e713695167f131668b987f1af
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-19 23:07:43 +0000

    montador mips

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 n = int(input())
-b = list(map(int, input().split()))
-f = []
-f.append(b[0])
-end = 0
-for i in range(len(b) - 1):
-    if b[i] - b[i + 1] == 1:
-      b[i] = 0
-print(*b)
+for i in range(n):
+  a, b = map(int, input().split())
+  print(a + b)
